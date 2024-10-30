e6448f27c650f49521f805434bdec7825de3b7e3
Xander <121578706+Xandervile@users.noreply.github.com>
2024-10-30 08:10:51 +0000

    Update MysteryMaker.py

--- MysteryMaker.py
+++ MysteryMaker.py
@@ -647,6 +647,7 @@ def GenerateMysterySettings(inputFilename, fairyFilename, remainsFilename, fairy
         itemListString = AddStringToListString(itemListString,
                                                "---------1ffff-8000ffff-fdef7800-7fffffc---7f37ffe--1e7-fffc2cff-fffffeff-80000000-f041fff-ffb00183-c3003e00--------------")
         settings["OverrideHintPriorities"][0].remove("MaskScents")
+	junkListString = AddEntryToListString(junkListString, 2, "40000")
         gossipHintsTakenByAlways -= 1
         hardOptions += 1
     if catLooseRupees[0] != "No change":
@@ -679,7 +680,7 @@ def GenerateMysterySettings(inputFilename, fairyFilename, remainsFilename, fairy
     if catPotsanity[0] == "All but fairies/owls shuffled":
         itemListString = AddStringToListString(itemListString,
                                                "-----60--c000000-----3-f03f0000-3c107ff-e0000000-20804fff-fffffe18-100--707d2801-f0f3e000-4e0004-e3c186-b3fdef3-dc000000------------")
-        itemListString = RemoveEntryFromListString(itemListString, 0, "40000")
+        junkListString = AddEntryToListString(junkListString, 0, "40000")
         settings["OverrideHintPriorities"][0].remove("ItemBottleGoronRace")
         settings["OverrideHintPriorities"][1].append("CollectableDekuShrineGreyBoulderRoomPot1")
         gossipHintsTakenByAlways -= 1
@@ -735,6 +736,7 @@ def GenerateMysterySettings(inputFilename, fairyFilename, remainsFilename, fairy
         catMinigamesExtra = ["All three"]
         hardOptions += 1
     if catMinigames[0] != "No change":
+	settings["DoubleArcheryRewards"] = True
         nonzeroCategories += 1
         
     wgtsBombersNotebook = settingsweights["NotebookShuffle"][1]
